8009ff591e7c0359a45cc68be0518e13aa09044b
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2021-01-24 10:06:12 -0600

    Initial commit

---
+++ src/main/java/com/gkoliver/nozombifiedpiglins/NoZombifiedPiglins.java
@@ -0,0 +1,15 @@
+package com.gkoliver.nozombifiedpiglins;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("nozombifiedpiglins")
+public class NoZombifiedPiglins {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public NoZombifiedPiglins() {}
+}
---
+++ src/main/java/com/gkoliver/nozombifiedpiglins/mixin/NetherPortalBlockMixin.java
@@ -0,0 +1,24 @@
+package com.gkoliver.nozombifiedpiglins.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(NetherPortalBlock.class)
+public class NetherPortalBlockMixin extends Block  {
+    public NetherPortalBlockMixin(Properties properties) {
+        super(properties);
+    }
+    @Inject(method="randomTick", at=@At(value="HEAD"), cancellable = true)
+    private void randomTickI(BlockState state, ServerWorld worldIn, BlockPos pos, Random random, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
